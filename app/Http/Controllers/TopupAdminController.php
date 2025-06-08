3d50b54635011c00e806aa71f06a46ad3fa9fe85
khoirularbi97 <khoirularbi729@gmail.com>
2025-06-08 13:38:34 +0700

    pembaruan topup admin

--- app/Http/Controllers/TopupAdminController.php
+++ app/Http/Controllers/TopupAdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Topup;
+use App\Models\Transaksi;
 use App\Models\User;
 use Midtrans\Snap;
 use Midtrans\Config;
@@ -64,6 +65,16 @@ class TopupAdminController extends Controller
             'amount' => $amount,
             'status' => 'pending',
             'order_id' =>$orderId,
+            
+        ]);
+        $transaksi = Transaksi::create([
+            'users_id' => $user->id,
+            'nama'   => $user->name,
+            'uid'   => $user->uid,
+            'jenis'   => 'debit',
+            'jumlah' => $amount,
+            'Status' => 1,
+            
             
         ]);
 
