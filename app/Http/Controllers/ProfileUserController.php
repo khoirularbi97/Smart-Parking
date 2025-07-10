d8f016081fcfddd1c19f31797deecae761030279
khoirularbi97 <khoirularbi729@gmail.com>
2025-07-10 18:46:55 +0700

    optimalisasi fitur riwayat parkir dan user

--- app/Http/Controllers/ProfileUserController.php
+++ app/Http/Controllers/ProfileUserController.php
@@ -8,8 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
-use Illuminate\Validation\Rule;
-
 class ProfileUserController extends Controller
 {
      /**
@@ -27,14 +25,7 @@ class ProfileUserController extends Controller
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
-        $request->user()->fill($request->validated([
-             'uid' => [
-                'nullable',
-                'string',
-                'max:255',
-                Rule::unique('users', 'uid')->ignore($id),
-            ],
-        ]));
+        $request->user()->fill($request->validated());
 
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
